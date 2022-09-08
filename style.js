82b4e52707c84bd80a41d15c36791f98a4d717ed
brucemorakane <brucemorakane74@gmail.com.com>
2022-09-08 14:01:10 +0200

    added function addclick

--- style.js
+++ style.js
@@ -1,3 +1,4 @@
+
 var button = "add cart"
 var addbutton = "addbutton"
 var name = "name"
@@ -6,3 +7,13 @@ var name = "name"
 document.getElementById('add cart')
 .innerHTML = button
 
+function button() {
+    for (let button = 0; button < array.length; button++) {
+        const element = array[button];
+        
+    }
+}
+
+function addclick() {
+    
+}
