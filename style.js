ced0ae462f63c1fbb02c2078140e19125d21b72c
brucemorakane <brucemorakane74@gmail.com.com>
2022-09-12 11:32:21 +0200

    delete function addclick

--- style.js
+++ style.js
@@ -7,19 +7,14 @@ var name = []
 document.getElementById('add cart')
 .innerHTML =  name
 
+document.getElementById('addbutton')
+.innerHTML = name 
+
 function button() {
     for (let  name = 0;  name < array.length;  name++) {
         const element = array[ name];
         
     }
-}
-
-function addclick() {
-    if ( name) {
-        return
-    
-    } false
-    return
 }
 
 allScreen()
